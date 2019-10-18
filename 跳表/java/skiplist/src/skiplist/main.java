de6ece98a3abf1d60f3083d144f422f256c7d94d
itleeyw <itleeyw@gmail.com>
2019-10-19 06:55:52 +0800

    跳表

---
+++ 跳表/java/skiplist/src/skiplist/main.java
@@ -0,0 +1,17 @@
+package skiplist;
+
+import skiplist.SkipList.Node;
+
+public class main {
+
+	public static void main(String[] args) {
+		  final float SKIPLIST_P = 0.5f;
+		  final int MAX_LEVEL = 16;
+		// TODO Auto-generated method stub
+		SkipList skiplist = new SkipList();
+		Node h = skiplist.new Node(); 
+		int level = 1;
+		double c;
+	}
+
+}
